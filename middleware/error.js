afc6ba2718d02484bc2ed817999cbf519769deae
Lars Wolter <lwolter1337@gmail.com>
2022-12-14 15:01:07 +0100

    Added error logging to mongo db with metadata

--- middleware/error.js
+++ middleware/error.js
@@ -5,16 +5,18 @@ const logpath = './logs/';
 
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
+    format: winston.format.json(), 
+    defaultMeta: { 
+      service: 'user-service'
+    },
     transports: [
-      new winston.transports.File({ filename: logpath + 'error.log', level: 'error'}),
-      new winston.transports.File({ filename: logpath + 'combined.log'}),
-      new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly'})
+      new winston.transports.File({ filename: logpath + 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: logpath + 'combined.log', level: 'info' }),
+      new winston.transports.MongoDB({ db: 'mongodb://localhost/vidly', level: 'info'})
     ]
   });
 
 module.exports = function(err, req, res, next) {
-    logger.error(err.message, err);
-    res.status(500).send('Something failed.');
-}
+  logger.info(err.message, { metadata: { error: err } });
+  res.status(500).send('Something failed.');
+};
